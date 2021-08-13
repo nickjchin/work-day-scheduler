b268adb28f8715beb1d0d46feb15542dd6f1bd62
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-08-13 15:44:51 -0400

    added more rows and buttons in html and js to see that only the first row worked

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,30 @@
 $(document).ready(function () {
   // All of our code is ran in here
 
-  var saveButtonEl = $("#save-button");
+  var saveButton9 = $("#save-button-9");
+  var saveButton10 = $("#save-button-9");
+  var saveButton11 = $("#save-button-9");
+  var saveButton12 = $("#save-button-9");
 
-  // function listen for click events on the page
-  saveButtonEl.on("click", function () {
+  // 9AM function listen for click events on the page
+  saveButton9.on("click", function () {
     // grab the saved event options
+    //create variable to save user input and time
+    var time = moment().format("hA");
+    var userInput = document.querySelector(".user-input").value;
+    console.log(userInput);
+    // save to localStorage
+    localStorage.setItem("event", JSON.stringify({ time: time, event: userInput }));
+    // Alert user that items have been saved to localStorage.
+    var localStorageAlert = document.getElementById("alert");
+    localStorageAlert.innerText = "Event saved to local storage \u2713";
+    // create some kind of timeOut value that removes the notification
+    $("#alert").fadeIn("slow").delay(3000).fadeOut("slow");
+  });
 
+  // 10AM function listen for click events on the page
+  saveButton10.on("click", function () {
+    // grab the saved event options
     //create variable to save user input and time
     var time = moment().format("hA");
     var userInput = document.querySelector(".user-input").value;
@@ -26,12 +44,13 @@ $(document).ready(function () {
   var currentTime = moment().format("LT");
   // create our loop to go over all the time blocks (rows)  -  each row has id indicating which hour
 
+  //   for()
+
   // if else if statement to:
   // ().removeClass
   // check to see if we have passed our time
   // check out removeClass and addClass
   // check to see if current hour (current time var) matches physcial hour (ids)
-
   // check to see if the times is in the future
 
   // Retrieve localStorage and render items to the correct rows    // could copy paste 10 times if one row works
