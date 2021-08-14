5774434014f4ea424d7c8dcc5c9f89d4dff742f7
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-08-13 20:01:29 -0400

    got button 9 and 10 to store values in localStorage

--- assets/script.js
+++ assets/script.js
@@ -7,16 +7,69 @@ $(document).ready(function () {
   // var saveButton11 = $("#save-button-9");
   // var saveButton12 = $("#save-button-9");
 
-  // 9AM function listen for click events on the page
-  saveButton9.on("click", function () {
+
+
+  // 9AM FUNCTION: listen for click events on the page
+    saveButton9.on("click", function(event) {
+      event.preventDefault();
     // grab the saved event options
     //create variable to save user input and time
-    // var time = moment().format("hA");
+    var arr = []
     var time = 9;
-    var userInput = document.querySelector(".user-input").value;
+    var userInput = document.querySelector(".user-input-9").value;
+    console.log(userInput);
+
+    // save to localStorage
+    var existingEvents = JSON.parse(localStorage.getItem("allEvents"));
+    if (existingEvents == null) existingEvents = [];
+    var userPlannedEvent = { time: time, event: userInput };
+    localStorage.setItem("events", JSON.stringify(userPlannedEvent));
+    existingEvents.push(userPlannedEvent);
+    localStorage.setItem("allEvents", JSON.stringify(existingEvents));
+
+
+    // var existingUserInfo = JSON.parse(localStorage.getItem("allScores"));
+    // if (existingUserInfo == null) existingUserInfo = [];
+    // var userInitials = document.getElementById("initials").value;
+    // var userHighScoreInfo = { initials: userInitials, score: scoreCounter };
+    // localStorage.setItem("userHighScoreInfo", JSON.stringify(userHighScoreInfo));
+    // // store allScores to localStorage
+    // existingUserInfo.push(userHighScoreInfo);
+    // localStorage.setItem("allScores", JSON.stringify(existingUserInfo));
+
+
+
+      // Need to check to see if array updates with button 10 clicks
+
+    // Alert user that items have been saved to localStorage.
+    var localStorageAlert = document.getElementById("alert");
+    localStorageAlert.innerText = "Event saved to local storage \u2713";
+    // create some kind of timeOut value that removes the notification
+    $("#alert").fadeIn("slow").delay(3000).fadeOut("slow");
+  });
+  
+
+
+
+  // 10AM FUNCTION: listen for click events on the page
+  saveButton10.on("click", function () {
+    // grab the saved event options
+    //create variable to save user input and time
+    var time = 10;
+    var arr = [];
+    var userInput = document.querySelector(".user-input-10").value;
     console.log(userInput);
     // save to localStorage
-    localStorage.setItem("event", JSON.stringify({ time: time, event: userInput }));
+    var existingEvents = JSON.parse(localStorage.getItem("allEvents"));
+    if (existingEvents == null) existingEvents = [];
+    var userPlannedEvent = { time: time, event: userInput };
+    localStorage.setItem("events", JSON.stringify(userPlannedEvent));
+    existingEvents.push(userPlannedEvent);
+    localStorage.setItem("allEvents", JSON.stringify(existingEvents));
+
+
+
+    // localStorage.setItem("event", JSON.stringify({ time: time, event: userInput }));
     // Alert user that items have been saved to localStorage.
     var localStorageAlert = document.getElementById("alert");
     localStorageAlert.innerText = "Event saved to local storage \u2713";
@@ -24,21 +77,6 @@ $(document).ready(function () {
     $("#alert").fadeIn("slow").delay(3000).fadeOut("slow");
   });
 
-  // 10AM function listen for click events on the page
-  saveButton10.on("click", function () {
-    // grab the saved event options
-    //create variable to save user input and time
-    var time = 10;
-    var userInput = document.querySelector(".user-input").value;
-    console.log(userInput);
-    // save to localStorage
-    localStorage.setItem("event", JSON.stringify({ time: time, event: userInput }));
-    // Alert user that items have been saved to localStorage.
-    var localStorageAlert = document.getElementById("alert");
-    localStorageAlert.innerText = "Event saved to local storage \u2713";
-    // create some kind of timeOut value that removes the notification
-    $("#alert").fadeIn("slow").delay(3000).fadeOut("slow");
-  });
 
   // create time updater function()
   // get current time
